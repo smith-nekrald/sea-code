684d3979d6997093372df9d02588a0d5bb3cdc35
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-04 21:11:31 -0400

    Added entry point script. Reorganization with modules launch and process.

---
+++ run_system.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# A one script to launch the entire system.
+
+set -uexo pipefail
+
+script_path=`readlink -f "${BASH_SOURCE[0]}"`
+script_dir=`dirname "$script_path"`
+code_root=`readlink -f "$script_dir"`
+
+system_root="$code_root"/system
+"$system_root"/setup.sh
+
+launch_root="$code_root"/launch
+"$launch_root"/sample_datasets.sh
+"$launch_root"/ufgm_with_split.sh
+
+process_root="$code_root"/process
+"$process_root"/make_info_storage.sh
+"$process_root"/create_images.sh
+
+set +uexo
