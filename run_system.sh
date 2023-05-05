404b19bb0fbc883cbc677ea90ccdb1a354bdc9f5
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-05 16:11:14 -0400

    Moved submodule commits into a modern version.

--- run_system.sh
+++ run_system.sh
@@ -11,6 +11,9 @@ code_root=`readlink -f "$script_dir"`
 system_root="$code_root"/system
 "$system_root"/setup.sh
 
+presets_root="$code_root"/presets
+"$presets_root"/fix_configurations.py
+
 launch_root="$code_root"/launch
 "$launch_root"/sample_datasets.sh
 "$launch_root"/ufgm_with_split.sh
@@ -18,5 +21,6 @@ launch_root="$code_root"/launch
 process_root="$code_root"/process
 "$process_root"/make_info_storage.sh
 "$process_root"/create_images.sh
+"$process_root"/visualize_inputs.sh
 
 set +uexo
